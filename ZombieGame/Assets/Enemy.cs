c9f99f1dbd2d9723ca7609d06c592496428b07c4
Aman Kesarwani <amank.iitg@gmail.com>
2023-06-10 20:02:58 -0400

    script

---
+++ ZombieGame/Assets/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform goal;
+    private NavMeshAgent agent;
+    void Start()
+    {
+        goal = Camera.main.transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+
+        GetComponent<Animation>().Play("Z_Walk_InPlace");
+        Debug.Log("Started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        goal = Camera.main.transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    public void zombie_dead()
+    {
+        Debug.Log("Triggered");
+        agent.isStopped = true;
+        GetComponent<AudioSource>().Play();
+        GetComponent<Animation>().Stop();
+        GetComponent<Animation>().Play("Z_FallingBack");
+        Destroy(gameObject, 2);
+        GameObject zombie = Instantiate(Resources.Load("Zombie", typeof(GameObject))) as GameObject;
+        float randomX  = UnityEngine.Random.Range(-64f,64f);
+        float constantY = 0.01f;
+        float randomZ = UnityEngine.Random.Range(30f,120f);
+        zombie.transform.position = new Vector3(randomX,constantY,randomZ);
+        while( Vector3.Distance(zombie.transform.position, Camera.main.transform.position)<=10){
+            randomX  = UnityEngine.Random.Range(-64f,64f);
+            randomZ  = UnityEngine.Random.Range(30f,120f);
+            zombie.transform.position = new Vector3(randomX,constantY,randomZ);
+        }
+    }
+    
+    public void call_gun()
+    {
+        GameObject player = GameObject.Find("Player");
+        Player player_controller = player.GetComponent<Player>();
+        player_controller.shoot(); 
+    }
+
+}
+
+
---
+++ ZombieGame/Assets/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+
+public class Player : MonoBehaviour
+{
+
+    public TextMeshProUGUI textMeshProUGUI;
+    private int score;
+
+    public Transform vrCamera;
+    public float angle=30.0f;
+    public float speed=-40.0f;
+    public bool moveForward;
+
+    private CharacterController cc;
+    //private bool movement = true;
+    private GameObject gun;
+
+    public void shoot()
+    {
+        Debug.Log("shoot");
+        score +=1;
+        StartCoroutine("Shoot");
+    }
+
+    IEnumerator Shoot()
+    {
+        gun.GetComponent<Animation>().Play();
+        yield return new WaitForSeconds(1f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        textMeshProUGUI.text = "Score: " + score;
+        cc = GetComponent<CharacterController>();
+        gun = gameObject.transform.GetChild(0).gameObject.transform.GetChild(1).gameObject;
+    }
+
+    
+
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMeshProUGUI.text = "Score: " + score;
+        if(vrCamera.eulerAngles.x>angle && vrCamera.eulerAngles.x < 90.0f){
+            moveForward=true;
+        } else{
+            moveForward=false;
+        }
+
+        if(moveForward){
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+            //console.log('Forward');
+            cc.SimpleMove(speed*forward);
+        }
+    }
+
+    
+}
